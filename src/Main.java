898b405482fc621dc7c8d86444bb87a46d8d4601
hyun-98 <126747395+hyun-98@users.noreply.github.com>
2025-06-10 09:46:20 +0900

    클래스와 객체

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
+class Person {
+    String name;
+    int age;
+
+    public void sayHello() {
+        System.out.println("안녕하세요? 저는 " + name + "입니다. 저의 나이는  " + age + "살 입니다.");
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
-        String message = "Hello World!";
+        Person person = new Person();
 
-        //문자열 길이 확인
-        System.out.println("문자열 길이 : " + message.length());
-
-        //대문자로 변환
-        System.out.println("대문자로 변환 : " + message.toUpperCase());
-
-        //부분 문자열 추출
-        System.out.println("부분 문자열 (0-5) : " + message.substring(0, 5));
-
-        //특정 문자의 위치 찾기
-        System.out.println("문자 'W'의 위치 : " + message.indexOf('W'));
+        person.name = "조현우";
+        person.age = 28;
+        person.sayHello();
     }
 }
